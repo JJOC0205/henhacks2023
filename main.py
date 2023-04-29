a89d76c6459fc18243f662204971027ba253d852
rrachelr <rachrobins1@gmail.com>
2023-04-29 15:25:57 -0400

    Wepage fixes

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ with ui.tabs() as tabs:
 with ui.tab_panels(tabs, value="Home"):
     with ui.tab_panel("Home"):
         ui.markdown('''##Welcome to HenHub!''')
-        ui.label("")
-        ui.input(label='Enter your canvas id:')
+        ui.label('''To learn how to access your Canvas User ID, check out our help tab!\n
+                 Remember: Do not share your User ID with anyone!''')
+        ui.input(label='Enter your Canvas ID:')
     with ui.tab_panel("About"):
-        ui.label("Connect with students taking similar courses")
+        ui.label("Connect with students taking similar courses!")
 
 
 ui.run()
