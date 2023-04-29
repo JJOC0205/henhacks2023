8e19cc08c45520b5a47de4999c07771ca2753bbc
Jon O'Connell <jjoc0205@gmail.com>
2023-04-29 16:42:39 -0400

    course work

--- canvas_api.py
+++ canvas_api.py
@@ -1,9 +1,29 @@
-from canvasapi import Canvas
+#from canvasapi import Canvas
+import requests
+import datetime
+from bakery_canvas import get_courses
 
-API_URL = ""
-API_KEY = ""
+API_URL = "https://udel.instructure.com"
+API_KEY = "25~dUCIgmpuYNRJDiQ5YFIjRQb5vcSrtWaWBAd1TL1IUfHkvHrbT5BGe4nKNwiEIcfK"
 
-canvas = Canvas(API_URL,API_KEY)
+courses = get_courses(API_KEY)
 
-course = canvas.get_course(123456)
-course.name
+def print_courses(courses):
+    for course in courses:
+       print(course.name) 
+    return
+def current_courses(courses):
+    spring23 = datetime.datetime(2023,1,24)
+    current = []
+    for course in courses:
+        if course.end_at:
+            course.end_at = course.end_at[0:10]
+            year = int(course.end_at[0:4])
+            month = int(course.end_at[6:7])
+            day = int(course.end_at[9:10])
+            as_datetime = datetime.datetime(year,month,day)
+            if as_datetime > spring23:
+                current.append(course)
+    return current
+#print_courses(courses)
+print(current_courses(courses))
