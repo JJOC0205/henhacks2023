c1c57bfda663c363fae68b4a454292908a1cd762
rrachelr <rachrobins1@gmail.com>
2023-04-29 23:07:41 -0400

    lol

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from nicegui import ui    
 import canvas_api
 
-def set_apikey(key:str):
-    canvas_api.create_dict("Rachel", key)
+api_key = []
 
+'''def set_apikey(key:str):
+    canvas_api.create_dict("Rachel", key)'''
+    
 with ui.tabs() as tabs:
     ui.tab("Home", icon="home")
     ui.tab("About", icon="info")
@@ -14,7 +16,7 @@ with ui.tab_panels(tabs, value="Home"):
     with ui.tab_panel("Home"):
         ui.markdown("#Welcome to HenHub!")
         ui.label("To learn how to generate your Canvas access token, check out our help tab!\nRemember: Do not share your token with anyone!")
-        ui.input(label='Enter your Canvas access token:', on_change=lambda e: set_apikey(e.value))
+        ui.input(label='Enter your Canvas access token:', on_change=lambda e: api_key.append(e.value))
     with ui.tab_panel("About"):
         ui.label("Connect with students taking similar courses!")
     with ui.tab_panel("Help"):
